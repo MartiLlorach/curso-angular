16479492a46f957d9db923038bda628dbe0f6033
mllorach <martillorachnaharro@gmail.com>
2022-01-17 08:37:44 +0100

    importacion del proyecto

---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Country } from '../interfaces/pais.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor( private http: HttpClient) { }
+
+  buscarPais(pais: string): Observable<Country[]> {
+    
+    const url = `${this.apiUrl}/name/${pais}`;
+    return this.http.get<Country[]>(url);
+
+  }
+
+  buscarCapital(capital: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/capital/${capital}`;
+    return this.http.get<Country[]>(url);
+
+  }
+
+}
